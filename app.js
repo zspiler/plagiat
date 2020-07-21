3f0b15df1bc421c2c087cacde030b87a871b0450
zanspiler <zanspiler1@gmail.com>
2020-07-21 18:33:30 +0200

    Debugging..

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use('/api/tests', require('./api/tests'));
 
 
 if (process.env.NODE_ENV == 'production') {
+    console.log(`production!`);
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
